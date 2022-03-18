de447b78bf2b28c8a087acb7ba1284470bca7598
Kaium456 <kaiumbd456@gmail.com>
2022-03-18 15:55:44 +0600

    all code done

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import "./App.css";
 import photo from "./img/logo.png";
 function App() {
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-200">
-      <div className="max-w-sm mx-auto p-8 rounded-xl bg-white shadow-md space-y-2 sm:flex sm:items-center sm:px-4 sm:space-y-0 sm:space-x-6">
+    <div className="h-screen flex items-center justify-center bg-gray-200 dark:bg-gray-900">
+      <div className="max-w-sm mx-auto p-8 rounded-xl bg-white shadow-md space-y-2 sm:flex sm:items-center sm:px-4 sm:space-y-0 sm:space-x-6 dark:bg-gray-800 sm:dark:hover:bg-gray-400 sm:dark:duration-1000">
         <img
           src={photo}
           alt=""
@@ -11,12 +11,15 @@ function App() {
         />
         <div className="space-y-0.5 sm:text-left">
           <div className="text-center ">
-            <p className="text-lg text-black font-semibold">Md Kaium Islam</p>
-            <p className="text-gray-500 font-medium ">Programmer</p>
+            <h1 className=" text-black font-semibold dark:text-gray-200">
+              Md Kaium Islam
+            </h1>
+            <p className="text-gray-500 font-medium dark:text-gray-400 sm:dark:text-red-200">
+              Programmer
+            </p>
           </div>
-          <button className="mx-6 px-4 py-1 border border-purple-200 rounded-full text-sm text-purple-600 font-semibold hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
-            Visit Now
-          </button>
+          <button className="btn btn-purple ">Visit Now</button>
+          <button className="btn btn-green">Website</button>
         </div>
       </div>
     </div>
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const Button = () => {
-    return (
-        <div>
-            <button>Click Me</button>
-        </div>
-    );
+  return (
+    <div>
+      <button className="p-4 py-1 bg-lime-600 rounded-full font-bold text-white">
+        Click Me
+      </button>
+      <br />
+      <br />
+      <button className="p-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-600 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+        Message
+      </button>
+    </div>
+  );
 };
 
 export default Button;
